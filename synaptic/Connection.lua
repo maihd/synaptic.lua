b82a3809062e10a9baa3875a28280284c910f8c5
maihd <maihd.dev@gmail.com>
2023-01-05 14:35:19 +0700

    Connection

---
+++ synaptic/Connection.lua
@@ -0,0 +1,27 @@
+local utils = dofile("utils.lua")
+
+local Connection = utils.class("Connection")
+
+function Connection:__init__(from, to, weight)
+    if not from or not to then
+        error("Connection Error: Invalid neurons")
+    end
+
+    self.id     = Connection.uid()
+    self.from   = from
+    self.to     = to
+    self.weight = weight or math.random(-0.1, 0.1)
+    self.gain   = 1
+    self.gater  = nil
+end
+
+-- Static
+Connection.connections = 0
+function Connection.uid()
+    local id = Connection.connections
+    Connection.connections = Connection.connections + 1
+    return id
+end
+
+-- Exports
+return Connection
